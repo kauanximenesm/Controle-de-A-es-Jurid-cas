efe5747880ff75a72165978f0348a7ca34ae725c
kauanximenesm <138837914+kauanximenesm@users.noreply.github.com>
2026-05-10 22:26:17 -0300

    crud 1.4

--- Js/main.js
+++ Js/main.js
@@ -19,3 +19,15 @@ function gerarRelatorio() {
     alert("Relatório PDF sendo gerado...");
     painel.innerHTML = "📄 Relatório de Março exportado com sucesso.";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pega o perfil que salvamos no localStorage durante o login
+    const perfil = localStorage.getItem('perfilUsuario'); 
+
+    console.log("Perfil atual logado:", perfil); // Isso vai nos mostrar no F12 se o login está salvando certo
+
+    if (perfil === '5') {
+        // Se for Admin, a gente mostra o botão
+        document.getElementById('btnAdminGestao').style.display = 'flex';
+    }
+});
