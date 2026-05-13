ddf869660a1f3a917420bda3be2cc7ba13888381
kauanximenesm <138837914+kauanximenesm@users.noreply.github.com>
2026-05-12 22:52:41 -0300

    crud 10.1

--- Js/listapessoas.js
+++ Js/listapessoas.js
@@ -1,6 +1,6 @@
 async function carregarPessoas() {
     try {
-        const response = await fetch('https://controle-de-a-es-jur-cas.onrender.com/lista-pessoas', {
+        const response = await fetch('https://controle-de-a-es-jur-cas.onrender.com', {
             method: 'POST'
         });
         
